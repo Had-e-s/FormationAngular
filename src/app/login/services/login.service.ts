710bd93ca1c6389ce9a897da6aa0e29cea70a1de
hades <arnaud-ali@outlook.fr>
2018-09-28 12:00:13 +0200

    Log service

---
+++ src/app/login/services/login.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  public user = 'Alpha';
+  public log$ = new BehaviorSubject('logout');
+  constructor() { }
+
+  public login(): void {
+    this.log$.next('login');
+  }
+
+  public logout(): void {
+    this.log$.next('logout');
+  }
+}
--- src/app/ui/components/header/header.component.ts
+++ src/app/ui/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faSignInAlt, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { BehaviorSubject } from 'rxjs';
+import { LoginService } from '../../../login/services/login.service';
 
 @Component({
   selector: 'app-header',
@@ -12,14 +14,30 @@ export class HeaderComponent implements OnInit {
   public title = 'CRM Apps';
   public faBars = faBars;
   public faUser = faUser;
+  public faSignInAlt = faSignInAlt;
+  public faSignOutAlt = faSignOutAlt;
   public open = true;
+  public user: string;
+  public log$: BehaviorSubject<string>;
 
-  constructor() { }
+  constructor(
+    private loginService: LoginService
+  ) { }
 
   ngOnInit() {
+    this.log$ = this.loginService.log$;
+    this.user = this.loginService.user;
   }
 
   public toggle() {
     this.open = !this.open;
+  }
+
+  public login(): void {
+    this.loginService.login();
+  }
+
+  public logout(): void {
+    this.loginService.logout();
   }
 }
